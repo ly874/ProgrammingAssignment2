470e1eb79e879cd50a534fefcff4ef5d71d36e4c
ly874 <sdly874@gmail.com>
2014-12-15 22:13:54 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Well, my function is just imitating the sample of mean calculation, just substituted the class
+## "numeric" with "matrix" and the function mean() with solve().
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        InvMtrx<-NULL    ##Initialize InvMtrx = NULL        
+        set<-function(y=matrix()) {
+                x <<- y
+                InvMtrx <<- NULL
+        }  
+        get <- function() x
+        setInvMtrx <- function(Inv) InvMtrx <<- Inv
+        getInvMtrx <- function() InvMtrx
+        list(set=set, get=get, setInvMtrx=setInvMtrx, getInvMtrx=getInvMtrx)
 
 }
 
 
-## Write a short comment describing this function
+## A function that makes cache of matrix. In fact it returns a list of function: 
+## 1. set matrix 2. get matrix 3. set inversed matrix 4. get inversed matrix
 
 cacheSolve <- function(x, ...) {
+        InvMtrx <- x$getInvMtrx()
+        if (!is.null(InvMtrx)) {
+                message("getting cached inversed matrix")
+                return(InvMtrx)
+        }
+        ## To check if the inversed matrix has been calculated. If yes, ruturn it.
+        data <- x$get()
+        InvMtrx<-solve(data,...)
+        InvMtrx
         ## Return a matrix that is the inverse of 'x'
 }
